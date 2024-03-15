e98d451761b75f0762f5e18f45001cce7786613d
MAGI <magi@Bureau-Mourad.local>
2024-03-15 05:14:27 +0100

    first commit

---
+++ app/providers/AuthProvider.tsx
@@ -0,0 +1,10 @@
+import { ClerkProvider } from '@clerk/nextjs'
+import React from 'react'
+
+function AuthProvider() {
+  return (
+    <div>AuthProvider</div>
+  )
+}
+
+export default AuthProvider
